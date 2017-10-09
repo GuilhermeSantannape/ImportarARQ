7aea131bb5bd12e304fbf58dbe44c073e6568155
Guilherme Santanna <guilhermesantanna.mail@gmail..com>
2017-10-09 10:22:03 -0300

    Incluindo novo projeto com classe principal que importar arquivo em formato CSV

---
+++ src/Principal.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+public class Principal {
+
+	public static void main(String[] args) {
+		String arquivocvs =	"C:\Users\guilherme santanna\workspace\ImportarARQ\src\arquivo.csv";
+
+				
+	BufferedReader conteudoCSV=null;
+		
+	String linha = "";
+	
+	String csvSeparadorCompo =";";
+	
+	conteudoCSV = new BufferReader(new FileReader(arquivocvs))
+			
+			whil(true){
+		String[] moeda = Linha.split(csvSeparadorCompo);
+		
+		System.out.print("data="moeda[0]
+				+", Col 1= "+ moeda+[]);
+		
+		
+	
+	}
+	}
+
+}
