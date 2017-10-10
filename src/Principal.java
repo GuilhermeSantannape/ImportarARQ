4fefb585825382ba84721e3460df203302f26570
Guilherme Santanna <guilhermesantanna.mail@gmail..com>
2017-10-10 17:05:29 -0300

    ajustado classe principal para apenas importar  csv arquivo e lelo

--- src/Principal.java
+++ src/Principal.java
@@ -1,28 +1,19 @@
 import java.io.*;
+
 import java.util.*;
+
 public class Principal {
 
-	public static void main(String[] args) {
-		String arquivocvs =	"C:\Users\guilherme santanna\workspace\ImportarARQ\src\arquivo.csv";
-
-				
-	BufferedReader conteudoCSV=null;
-		
-	String linha = "";
-	
-	String csvSeparadorCompo =";";
-	
-	conteudoCSV = new BufferReader(new FileReader(arquivocvs))
-			
-			whil(true){
-		String[] moeda = Linha.split(csvSeparadorCompo);
-		
-		System.out.print("data="moeda[0]
-				+", Col 1= "+ moeda+[]);
-		
-		
-	
-	}
-	}
-
+	    public static void main(String[] args) throws IOException {
+	    	String arquivo="C:\\Users\\guilherme santanna\\workspace\\Hello\\src\\hello.csv\\";
+	    	InputStream is = new FileInputStream(arquivo);
+	    	InputStreamReader isr =  new InputStreamReader(is);
+	    	BufferedReader br = new BufferedReader(isr);
+	    	String s = br.readLine();
+	    	while( s!=null){
+	    		System.out.print(s);
+	    		s = br.readLine();
+	    		
+	    	}
+	    }
 }
