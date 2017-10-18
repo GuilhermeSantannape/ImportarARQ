8c206d10599465f4999def9c81d2fa11b8f87999
Guilherme Santanna <guilhermesantanna.mail@gmail..com>
2017-10-18 17:36:42 -0200

    Ajustado classe principal para seperar arquivo CSV por linha em colocar cada linha dentro de um arry.

--- src/Principal.java
+++ src/Principal.java
@@ -1,11 +1,53 @@
 import java.io.*;
 
-import java.util.*;
-
 public class Principal {
 
-	    public static void main(String[] args) throws IOException {
-	    	String arquivo="C:\\Users\\guilherme santanna\\workspace\\Hello\\src\\hello.csv\\";
+	  public static void main(String[] args) {
+
+		  Principal obj = new Principal();
+	    obj.run();
+
+	  }
+
+	  public void run() {
+
+	    String arquivoCSV = "C:\\\\Users\\\\guilherme santanna\\\\workspace\\\\ImportarARQ\\\\src\\\\Arquivo.csv\\";
+	    BufferedReader br = null;
+	    String linha = "";
+	    String csvDivisor = ",";
+	    try {
+
+	        br = new BufferedReader(new FileReader(arquivoCSV));
+	        while ((linha = br.readLine()) != null) {
+
+	            String[] Contato = linha.split(csvDivisor);
+
+	            System.out.println("contato [Nome= " + Contato[Contato.length-2] 
+	                                 + " , E-mail= " + Contato[Contato.length-1] + "]");
+
+	        }
+
+	    } catch (FileNotFoundException e) {
+	        e.printStackTrace();
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	        
+	        
+	    } finally {
+	        if (br != null) {
+	            try {
+	                br.close();
+	            } catch (IOException e) {
+	                e.printStackTrace();
+	            }
+	        }
+	    }
+	  }
+
+	}
+
+	/*    public static void main(String[] args) throws IOException {
+	    	String arquivo="C:\\Users\\guilherme santanna\\workspace\\ImportarARQ\\src\\Arquivo.csv\\";
 	    	InputStream is = new FileInputStream(arquivo);
 	    	InputStreamReader isr =  new InputStreamReader(is);
 	    	BufferedReader br = new BufferedReader(isr);
@@ -16,4 +58,4 @@ public class Principal {
 	    		
 	    	}
 	    }
-}
+}*/
